22e090b6876bd8b197192ae3199b9bdd324066a6
owen20108 <owen20108@yahoo.com>
2024-01-19 05:38:59 -0800

    config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
-# installs the package puppet-lint
-package {'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+# site.pp or your_module_name/manifests/init.pp
+
+class your_module_name {
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  package { 'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    require  => Package['python3-pip'],
+  }
 }
+
