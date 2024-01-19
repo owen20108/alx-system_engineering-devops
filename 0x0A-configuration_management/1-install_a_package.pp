c1c973347eec6a0d0253b0ea25ebaf8a6d5a6675
owen20108 <owen20108@yahoo.com>
2024-01-19 12:14:41 -0800

    pup

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,5 @@
-#!/usr/bin/pup
-# Ensure python3-pip is intialzied
-package { 'python3-pip' :
-  ensure => installed,
-}
-package {'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip',
-  require => package['python3-pip'],
-}
-package {'Werkzeug':
-  ensure   => '2.0.1',
-  provider => 'pip',
-  require => package['python3-pip'],
+#install puppet-lint v2.1.1
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
