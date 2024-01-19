b97341a4d9b6dd4e342061f578bf1874c20eaad5
owen20108 <owen20108@yahoo.com>
2024-01-19 03:26:24 -0800

    config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Using Puppet, install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+#install puppet-lint v2.1.1
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
