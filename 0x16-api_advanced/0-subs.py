155dd700f814b17743a4eaf8dada6e666d049120
owen20108 <owen20108@yahoo.com>
2024-03-08 15:58:43 -0800

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,42 +1,23 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
-If the subreddit is invalid or there is an issue, it returns 0.
-"""
-
+'''
+    this module contains the function number_of_subscribers
+'''
 import requests
+from sys import argv
+
 
 def number_of_subscribers(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'by u/owen20108'}  # Set a custom User-Agent to avoid issues
-
+    '''
+        returns the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'owen20108'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
     try:
-        response = requests.get(url, headers=headers)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            print(f"Error accessing subreddit '{subreddit}': HTTP status code {response.status_code}")
-            return 0
-
-    except requests.RequestException as e:
-        print(f"Error: {e}")
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
 
-if __name__ == '__main__':
-    import sys
 
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        num_subscribers = number_of_subscribers(subreddit)
-        
-        if num_subscribers != 0:
-            print(f"The subreddit '{subreddit}' has {num_subscribers} subscribers.")
-        else:
-            print(f"Unable to retrieve the number of subscribers for subreddit '{subreddit}'.")
-
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
