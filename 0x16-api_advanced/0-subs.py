52d4df74ca74c754943c0670cd81b90f95227b70
owen20108 <owen20108@yahoo.com>
2024-03-08 15:46:16 -0800

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 """
 Queries the Reddit API and returns the number of subscribers for a given subreddit.
-If the subreddit is invalid, it returns 0.
+If the subreddit is invalid or there is an issue, it returns 0.
 """
 
 import requests
-
 
 def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'by u/owen20108'}  # Set a custom User-Agent to avoid issues
 
-    response = requests.get(url, headers=headers)
+    try:
+        response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            print(f"Error accessing subreddit '{subreddit}': HTTP status code {response.status_code}")
+            return 0
+
+    except requests.RequestException as e:
+        print(f"Error: {e}")
         return 0
 
 if __name__ == '__main__':
@@ -28,4 +34,9 @@ if __name__ == '__main__':
     else:
         subreddit = sys.argv[1]
         num_subscribers = number_of_subscribers(subreddit)
-        print(num_subscribers)
+        
+        if num_subscribers != 0:
+            print(f"The subreddit '{subreddit}' has {num_subscribers} subscribers.")
+        else:
+            print(f"Unable to retrieve the number of subscribers for subreddit '{subreddit}'.")
+
